27d9264235cf86e751f73e42b2e218e4b77afbcb
chenyaoming <chenyaoming@lanjinrong.com>
2019-03-19 15:35:16 +0800

    'createTree算法优化'

--- utils/common.js
+++ utils/common.js
@@ -2,23 +2,36 @@
  * 创建二叉树
  * createTree([1, 2, 3]);
 */
-function nodeList(val) {
+function TreeNode(val) {
     this.val = val;
     this.left = this.right = null;
 }
 
-function createTree(arr, n = null) {
-    if (!arr.length) return null;
-    n = new nodeList(arr[0]);
-    if (typeof arr[1] === 'number') {
-        const res = [arr[1], ...arr.slice(3)];
-        n.left = createTree(res, n.left);
-    }
-    if (typeof arr[2] === 'number') {
-        const res = [arr[2], ...arr.slice(5)];
-        n.right = createTree(res, n.right);
+function init(m, n = null, row = 0, col = 0) {
+    if (m[row] && m[row][col]) {
+        n = new TreeNode(m[row][col]);
+        n.left = init(m, n.left, row + 1, (col * 2));
+        n.right = init(m, n.right, row + 1, (col * 2 + 1));
     }
     return n;
+}
+
+function createTree(arr) {
+    let map = [];
+    let drop = 0;
+    let index = 0;
+    while (index < arr.length) {
+        if (map[drop] === undefined) {
+            map[drop] = [arr[index]];
+        } else {
+            map[drop].push(arr[index]);
+        }
+        index += 1;
+        if (map[drop].length >= Math.pow(2, drop)) {
+            drop += 1;
+        }
+    }
+    return init(map);
 }
 
 module.exports = {
