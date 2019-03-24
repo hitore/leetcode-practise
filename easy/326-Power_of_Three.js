994fa30eebaed0c6660f5898534435d97f0c4bf5
MSI\47104 <15732685948@163.com>
2019-03-24 19:35:53 +0800

    'easy/326-Power-of-Three'

---
+++ easy/326-Power_of_Three.js
@@ -0,0 +1,12 @@
+/* 
+给定一个整数，写一个函数来判断它是否是 3 的幂次方。
+
+2019-03-24
+21038 / 21038 个通过测试用例
+执行用时 : 520 ms, 在Power of Three的JavaScript提交中击败了35.19% 的用户
+内存消耗 : 46.7 MB, 在Power of Three的JavaScript提交中击败了0.00% 的用户
+*/
+
+var isPowerOfThree = function(n) {
+    return /^10*$/.test(n.toString(3));
+};
