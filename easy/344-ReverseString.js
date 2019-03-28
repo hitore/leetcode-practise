7dc6411371c7f51450caf103c333afee0fe104ab
chenyaoming <chenyaoming@lanjinrong.com>
2019-03-28 17:18:27 +0800

    easy/344-ReverseString

---
+++ easy/344-ReverseString.js
@@ -0,0 +1,16 @@
+/* 
+编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+
+不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+
+你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+
+2019-03-28
+478 / 478 个通过测试用例
+执行用时 : 284 ms, 在Reverse String的JavaScript提交中击败了9.17% 的用户
+内存消耗 : 46.9 MB, 在Reverse String的JavaScript提交中击败了0.97% 的用户
+*/
+
+var reverseString = function(s) {
+    return s.reverse();
+};
