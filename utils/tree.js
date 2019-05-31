98ee1bac83575b58ab8d5c0aa9ae692dab523157
chenyaoming <chenyaoming@lanjinrong.com>
2019-05-31 15:52:34 +0800

    refactor: 修改算法

--- utils/tree.js
+++ utils/tree.js
@@ -7,13 +7,22 @@ function TreeNode(val) {
     this.left = this.right = null;
 }
 
-function createTree(arr, n = null, index = 0) {
-    if (arr[index] !== undefined) {
-        n = new TreeNode(arr[index]);
-        n.left = createTree(arr, n.left, (index * 2 + 1));
-        n.right = createTree(arr, n.right, (index * 2 + 2));
+function createTree(array) {
+    function done(arr, n = null, index = 0) {
+        if (arr[index] !== undefined && arr[index] !== null) {
+            n = new TreeNode(arr[index]);
+            n.left = done(arr, n.left, (index * 2 + 1));
+            n.right = done(arr, n.right, (index * 2 + 2));
+        }
+        return n;
     }
-    return n;
+    for (let i = 0; i < array.length; i += 1) {
+        let len = array.length - 1;
+        if (array[i] === null && i * 2 < len) {
+            array = [...array.slice(0, i * 2 + 1), null, null, ...array.slice(i * 2 + 1)];
+        }
+    }
+    return done(array);
 }
 
 module.exports = {
