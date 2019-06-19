f031f5b74f6f0dee715371e4fa45fe2554cae7ba
chenyaoming <chenyaoming@lanjinrong.com>
2019-06-19 18:36:52 +0800

    modifly note

--- middle/209-MinimumSizeSubarraySum.js
+++ middle/209-MinimumSizeSubarraySum.js
@@ -1,5 +1,5 @@
 /* 
-Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead
 
 Example: 
 
