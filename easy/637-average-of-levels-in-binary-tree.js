0980481edd0ea10104fc453418d59f3bca7276a8
chenyaoming <chenyaoming@lanjinrong.com>
2019-07-01 11:17:46 +0800

    637-average-of-levels-in-binary-tree

---
+++ easy/637-average-of-levels-in-binary-tree.js
@@ -0,0 +1,27 @@
+/* 
+给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a2 + b2 = c。
+
+2019-07-01
+65 / 65 个通过测试用例
+执行用时 : 128 ms , 在所有JavaScript提交中击败了 87.00% 的用户
+内存消耗 : 41.9 MB , 在所有JavaScript提交中击败了 5.62% 的用户
+*/
+
+var averageOfLevels = function(root) {
+    let res = [];
+    let nums = [];
+
+    function map(node, index = 0) {
+        if (node === null) return;
+        res[index] === undefined ? res[index] = node.val : res[index] += node.val;
+        nums[index] === undefined ? nums[index] = 1 : nums[index] += 1;
+        map(node.left, index + 1);
+        map(node.right, index + 1);
+    }
+
+    map(root);
+
+    return res.map((r, i) => {
+        return r / nums[i];
+    });
+};
