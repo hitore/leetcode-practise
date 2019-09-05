aa2eface25e3f038d05f6fc18e6154a80977d493
chenyaoming <chenyaoming@lanjinrong.com>
2019-09-05 18:39:04 +0800

    704

---
+++ easy/704-二分查找.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=javascript
+ *
+ * [704] 二分查找
+ */
+/*
+√ Accepted
+  √ 46/46 cases passed (80 ms)
+  √ Your runtime beats 89.46 % of javascript submissions
+  √ Your memory usage beats 37.3 % of javascript submissions (36.8 MB)
+*/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  
+  while(left <= right) {
+    let index = parseInt((left + right) / 2);
+    if (nums[index] === target) {
+      return index;
+    }
+    if (nums[index] > target) {
+      right = index - 1;
+    } else {
+      left = index + 1;
+    }
+  }
+  
+  return -1;
+};
+
