a24786327de1d7a8e8317e6399e55ab1ec0141d7
chenyaoming <chenyaoming@lanjinrong.com>
2019-09-11 18:39:38 +0800

    modifly note

--- easy/412-FizzBuzz.js
+++ easy/412-FizzBuzz.js
@@ -1,5 +1,5 @@
 /* 
-Write a program that outputs the string representation of numbers from 1 to n.
+Write a program that outputs the string representation of numbers from 1 to n
 
 But for multiples of three it should output “Fizz” instead of the number and for the multiples
 
