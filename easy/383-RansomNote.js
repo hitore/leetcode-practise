ac499f891d9ec34c836aa3cc397d61160628f8a9
chenyaoming <chenyaoming@lanjinrong.com>
2019-09-23 19:15:13 +0800

    modifly note

--- easy/383-RansomNote.js
+++ easy/383-RansomNote.js
@@ -1,5 +1,5 @@
 /* 
-给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，判断第一个字符串ransom能不能由第二个字符串magazines里面的字符构成。如果可以构成，返回 true ；否则返回 false。
+给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，判断第一个字符串ransom能不能由第二个字符串magazines里面的字符构成。如果可以构成，返回 true ；否则返回 false
 
 注意：
 
