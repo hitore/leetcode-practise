4f14c8a96384fb3e381ff0eca7dc17159fcb3e33
chenyaoming <15732685948@163.com>
2019-11-15 23:09:55 +0800

    modifly note

--- easy/506-RelativeRanks.js
+++ easy/506-RelativeRanks.js
@@ -1,6 +1,6 @@
 /* 
 Given scores of N athletes, find their relative ranks and the people with the top three highest scores,
-who will be awarded medals: "Gold Medal", "Silver Medal" and "Bronze Medal".
+who will be awarded medals: "Gold Medal", "Silver Medal" and "Bronze Medal"
 
 2019-05-30
 17 / 17 个通过测试用例
