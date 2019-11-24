c3dfe6b3d371c61a5b5e1337bac6d48ee500b076
chenyaoming <15732685948@163.com>
2019-11-24 21:29:50 +0800

    modifly note

--- easy/461-HammingDistance.js
+++ easy/461-HammingDistance.js
@@ -1,5 +1,5 @@
 /* 
-Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2
+Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
 
 2019-05-16
 149 / 149 个通过测试用例
